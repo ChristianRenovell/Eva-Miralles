f18c52d8085673f7d85cf21297ea6ea2384ca84d
Christian <christianrenovel83@gmail.com>
2021-02-20 09:03:25 +0000

    mensaje enamil

--- forms/contact.php
+++ forms/contact.php
@@ -1,21 +1,5 @@
 <?php
 
-if (isset($_POST['email'])) {
-  if (!empty($_POST['name']) && !empty($_POST['email']) 
-      && !empty($_POST['subject']) && !empty($_POST['message'])) {
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $subject = $_POST['subject'];
-        $message = $_POST['message'];
-        $header = "From: christianrenovell83@gmail.com" . "\r\n";
-        $header = "Reply-To: christianrenovell83@gmail.com" ."\r\n";
-        $header = "X-Mailer: PHP" . phpversion();
-        $mail = @mail($email,$subject,$message);
-        if($mail){
-          echo "<h4> enviado </h4>";
-        }else{
-          echo "<h4> fallo </h4>";
-        }
-  }
-}
+  echo "<h4> enviado </h4>";
+
 ?>
